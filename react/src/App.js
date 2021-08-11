92839e158c523287f2c2a661c183baadb1fde615
Joji Tenges <joji.tenges@prooftec.com>
2021-08-11 11:53:09 +1000

    make board cells

--- react/src/App.js
+++ react/src/App.js
@@ -2,21 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const BOARD_HEIGHT = 10;
+  const BOARD_WIDTH = 10;
+
+  // Make board cells
+  let board = Array(BOARD_HEIGHT * BOARD_WIDTH).fill(null);
+  let rows = [];
+  for (let rowIdx = 0; rowIdx < BOARD_HEIGHT; ++rowIdx) {
+    let startIdx = rowIdx * BOARD_WIDTH;
+    let rowCells = board
+      .slice(startIdx, startIdx + BOARD_WIDTH)
+      .map((val, idx) => <button className="cell" key={idx}/>);
+
+    rows.push(
+      <div className="cell-row">
+        {rowCells}
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {rows}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
