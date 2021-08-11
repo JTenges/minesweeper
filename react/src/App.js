3d891495eddc4a00bd7a3b6abb9fba5bf8fd1a53
Joji Tenges <joji.tenges@prooftec.com>
2021-08-11 13:14:17 +1000

    change cell color when clicked

--- react/src/App.js
+++ react/src/App.js
@@ -1,21 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+function Cell(props) {
+  let className = "Cell";
+  if (props.isRevealed) {
+    className += " Cell-revealed";
+  }
+  return <button className={className} onClick={props.handleClick} />;
+}
 
 function App() {
   const BOARD_HEIGHT = 10;
   const BOARD_WIDTH = 10;
+  // Create state
+  const [board, setBoard] = useState(Array(BOARD_HEIGHT * BOARD_WIDTH).fill(false));
+
+  // Select a cell
+  const revealCell = (idx) => {
+    const boardCpy = board.slice();
+    boardCpy[idx] = true;
+    setBoard(boardCpy);
+  };
 
   // Make board cells
-  let board = Array(BOARD_HEIGHT * BOARD_WIDTH).fill(null);
   let rows = [];
   for (let rowIdx = 0; rowIdx < BOARD_HEIGHT; ++rowIdx) {
     let startIdx = rowIdx * BOARD_WIDTH;
     let rowCells = board
       .slice(startIdx, startIdx + BOARD_WIDTH)
-      .map((val, idx) => <button className="cell" key={idx}/>);
+      .map((val, idx) => {
+        const cellIdx = startIdx + idx;
+        return (<Cell
+          key={cellIdx}
+          isRevealed={board[cellIdx]}
+          handleClick={() => revealCell(cellIdx)}/>);
+      });
 
     rows.push(
-      <div className="cell-row">
+      <div key={rowIdx} className="cell-row">
         {rowCells}
       </div>
     );
@@ -25,9 +48,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <div>
           {rows}
-        </p>
+        </div>
       </header>
     </div>
   );
